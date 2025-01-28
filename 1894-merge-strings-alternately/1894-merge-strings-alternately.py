05dc42972c639e84ed74ee78a0c660467d63e456
Yashwant-Mathur <168702587+Yashwant-Mathur@users.noreply.github.com>
2025-01-28 12:21:04 +0530

    Time: 48 ms (6.56%), Space: 17.7 MB (35.45%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        if not word1 :
+            return word2
+        if not word2 :
+            return word1
+        return word1[0]+word2[0]+ self.mergeAlternately(word1[1:],word2[1:])
