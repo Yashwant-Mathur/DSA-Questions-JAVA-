21a5652e224c5d126236e1c63cf38e392752acde
Yashwant Mathur <168702587+Yashwant-Mathur@users.noreply.github.com>
2025-04-12 12:38:26 +0530

    Time: 1 ms (37.93%), Space: 43 MB (88.45%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,26 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
-    int[] memo;
-    int[] cost;
-    // memozization
-    public int dp(int i) {
-        if (i >= cost.length) {
-            return 0;
-        }
-        if (memo[i] != -1) {
-            return memo[i];
-        }
-        memo[i] = Math.min(dp(i + 1), dp(i + 2)) + cost[i];
-        return memo[i];
-    }
-
     public int minCostClimbingStairs(int[] cost) {
-        this.cost = cost;
-        int n = cost.length;
-        memo = new int[n];
-        Arrays.fill(memo, -1);
+        //tabulation
+        int n=cost.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
 
-        return Math.min(dp(0), dp(1));
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return Math.min(dp[n-1],dp[n-2]);
+        
     }
 }
